4bab84cee7ac792d7aa3519d2092512d00b6d03f
Allen Jose Pius <allenjosesmailbox@gmail.com>
2019-06-17 01:22:53 +0530

    Update Product model   change sku to caseinsensitive field

--- product_importer/models.py
+++ product_importer/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django.contrib.postgres.fields import CICharField
 
 class Product(models.Model):
-	sku = models.CharField(max_length=200, unique=True)
+	sku = CICharField(max_length=200, unique=True)
 	name = models.CharField(max_length=200)
-	description = models.CharField(max_length=500, blank=True, null=True)
+	description = models.TextField(blank=True, null=True)
 	is_active = models.BooleanField(default=True)
