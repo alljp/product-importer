a9b37ec578b53cd7cafee01909cb326c0bbbd206
Allen Jose Pius <allenjosesmailbox@gmail.com>
2019-06-17 00:17:59 +0530

    Define Product model

--- product_importer/models.py
+++ product_importer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+	sku = models.CharField(max_length=200, unique=True)
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=500, blank=True, null=True)
+	is_active = models.BooleanField(default=True)
