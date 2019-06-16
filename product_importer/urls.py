e7136ac4f3a554253b68799fac878714803ecc7b
Allen Jose Pius <allenjosesmailbox@gmail.com>
2019-06-17 00:21:13 +0530

    Products view with basic pagination

--- product_importer/urls.py
+++ product_importer/urls.py
@@ -3,4 +3,5 @@ from product_importer import views
  
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
+    url(r'products', views.ProductsView.as_view())
 ]
--- product_importer/views.py
+++ product_importer/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from product_importer.models import Product
 
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+
+
+class ProductsView(ListView):
+    model = Product
+    template_name = 'products_list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+    queryset = Product.objects.all()
