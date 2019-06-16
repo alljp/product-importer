bcb010809bbb197d34890254955bbdf02268b4f9
Allen Jose Pius <allenjosesmailbox@gmail.com>
2019-06-16 21:27:30 +0530

    Setup postgres db

--- fulfil_assignment/settings.py
+++ fulfil_assignment/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'fulfil_assignment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fulfil',
+        'USER': 'postgres',
+        'PASSWORD': 'dsdbadmin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
