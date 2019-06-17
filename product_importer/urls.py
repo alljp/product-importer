060973fc5280d71b0c315201b6bb9a303a7bcba5
Allen Jose Pius <allenjosesmailbox@gmail.com>
2019-06-17 11:02:57 +0530

    Upload file

--- product_importer/urls.py
+++ product_importer/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from product_importer import views
  
+app_name = 'product_importer'
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    # url(r'^$', views.HomePageView.as_view()),
+    url(r'^$', views.upload_csv, name='upload_csv'),
     url(r'products', views.ProductsView.as_view())
 ]
--- product_importer/views.py
+++ product_importer/views.py
@@ -1,4 +1,9 @@
+import logging
+
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import TemplateView, ListView
 
 from product_importer.models import Product
@@ -16,3 +21,27 @@ class ProductsView(ListView):
     context_object_name = 'products'
     paginate_by = 10
     queryset = Product.objects.all()
+
+
+def upload_csv(request):
+    data = {}
+    if "GET" == request.method:
+        return render(request, "index.html", data)
+    # if not GET, then proceed
+    try:
+        print("PEYE")
+        csv_file = request.FILES["csv_file"]
+        print (csv_file)
+        if not csv_file.name.endswith('.csv'):
+            messages.error(request,'File is not CSV type')
+            return HttpResponseRedirect(reverse("product_importer:home"))
+        handle_uploaded_file(csv_file)
+    except Exception as e:
+        logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
+        messages.error(request,"Unable to upload file. "+repr(e))
+    return HttpResponseRedirect(reverse("product_importer:upload_csv"))
+
+
+def handle_uploaded_file(f):
+    for chunk in f.chunks():
+        pass
